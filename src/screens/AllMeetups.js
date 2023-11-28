cd4cd9ebb141157e4ae73c74982b0f99cce6afd9
Robbe De Geyter <robbe.degeyter@student.hogent.be>
2023-11-28 14:48:28 +0100

    Changed Home to AllMeetups

---
+++ src/screens/AllMeetups.js
@@ -0,0 +1,120 @@
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Keyboard, Touchable } from 'react-native'
+import { globalStyles } from '../styles/global'
+import { Modal } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { Ionicons, Entypo} from '@expo/vector-icons'; 
+import React, {useState} from 'react'
+import Card from './shared/Card'
+import uuid from 'react-native-uuid'
+import NewMeetup from './NewMeetup';
+
+
+const AllMeetups = (props) => {
+  const [meetups, setMeetups] = useState([
+    { title: "This is a first meetup", address: "Meetupstreet 1, 12345 Meetup City", description: "This is a first, amazing meetup which you definitely should not miss. It willbe a lot of fun!", favorite: false,  key: uuid.v4() },
+    { title: "This is a second meetup", address: "Meetupstreet 2, 12345 Meetup City", description: "This is a second, amazing meetup which you definitely should not miss. It willbe a lot of fun!", favorite: false,  key: uuid.v4() },
+  ]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const pressHandler = (item) => {
+    props.navigation.navigate("MeetupDetails", item);
+  };
+
+  const addMeetup = (review) => {
+    review.key = uuid.v4();
+    setMeetups((currentReviews) => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
+
+  const SetFavorite = (item) => {
+    setMeetups((prevMeetups) =>
+      prevMeetups.map((meetup) =>
+        meetup === item ? { ...meetup, favorite: !meetup.favorite } : meetup
+      )
+    );
+  };
+
+  const UnFavorite = (item) => {
+    setMeetups((prevMeetups) =>
+      prevMeetups.map((meetup) =>
+        meetup === item ? { ...meetup, favorite: !meetup.favorite } : meetup
+      )
+    );
+  };
+
+  return (
+    <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <TouchableWithoutFeedback
+          onPress={Keyboard.dismiss}
+        ></TouchableWithoutFeedback>
+        <View>
+          <MaterialIcons
+            name="close"
+            size={24}
+            onPress={() => setModalOpen(false)}
+            style={styles.modalToggle}
+          />
+          <NewMeetup addMeetup={addMeetup} />
+        </View>
+      </Modal>
+
+      <FlatList
+        data={meetups}
+        renderItem={({ item }) => (
+            <Card>
+              <View>
+                <Text style={globalStyles.titleText}>{item.title}</Text>
+              </View>
+              <TouchableOpacity>
+                <View style={styles.bottomContainer}>
+                  <Ionicons name="information-circle" size={30} color="cornflowerblue" onPress={() => pressHandler(item)}/>
+                  {item.favorite ? (
+                    <Ionicons name="heart-outline" size={30} color="indianred" onPress={() => UnFavorite(item)}/>
+                  ) : (
+                    <Ionicons
+                      name={item.favorite ? "heart-outline" : "heart-dislike-outline"}
+                      size={30}
+                      color="indianred"
+                      onPress={() => SetFavorite(item)}
+                    />
+                  )}
+                </View>
+              </TouchableOpacity>
+            </Card>
+        )}
+      />
+
+      <Entypo
+        name="add-to-list"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
+    </View>
+  );
+};
+
+export default AllMeetups;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    modalToggle:{
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    bottomContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+ });
